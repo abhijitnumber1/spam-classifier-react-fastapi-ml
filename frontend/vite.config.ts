70a6639871733ec580f227ca08cad19a48ea0a55
Abhijit Desktop <abhijitnumber1@gmail.com>
2025-10-07 01:11:06 +0530

    fixing vite path

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
+import react from "@vitejs/plugin-react";
+
+import path from "path";
+
+import tsconfigPaths from "vite-tsconfig-paths";
+
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react(), tsconfigPaths()],
+
+	base: "/spam-classifier-react-fastapi-ml/",
+
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+});
